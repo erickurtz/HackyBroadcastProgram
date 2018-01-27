64cd40afb8ccda710485ab16e3a180852864be68
Eric Kurtz <erickurtz@live.com>
2018-01-27 00:26:49 -0500

    added round button

--- hackyPythonProgram.py
+++ hackyPythonProgram.py
@@ -1,18 +1,23 @@
-import tkinter
+import tkinter as 
 
 global homeScore
 global awayScore
 
 homeScore = 0.0
 awayScore = 0.0
+roundNum = 0
 
 def writeToFile(): 
-	file = open("C:\\ourScore.txt", "w+")
+	file = open("ourScore.txt", "w+")
 	file.write(str(homeScore))
 	file.close()
-	file2 = open("C:\\theirScore.txt", "w+")
+	file2 = open("theirScore.txt", "w+")
 	file2.write(str(awayScore))
 	file2.close()
+
+        file3 = open("roundNum.txt", "w+")
+        file3.write(str(roundNum))
+        file3.close()
 
 def drawCallBack():
 	global homeScore
@@ -31,6 +36,9 @@ def theyWinCallBack():
 	awayScore += 1
 	writeToFile()
 	#increment their score by one
+def updateRoundNum():
+        roundNum+=1
+        writeToFile
 	
 writeToFile()
 
@@ -41,7 +49,10 @@ top = tkinter.Tk()
 win = tkinter.Button(top, text = "Win", command = brahWinCallBack)
 loss = tkinter.Button(top, text = "Loss", command = theyWinCallBack)
 draw = tkinter.Button(top, text = "Draw", command = drawCallBack)
+
+roundPlus = tkinter.Button(top, text = "+1 Round", command = updateRoundNum)
 win.pack()
 loss.pack()
 draw.pack()
+roundPlus.pack()
 top.mainloop() 
