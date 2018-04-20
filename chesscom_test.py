f3090a05b00973a583e9e2d6cc7e63e3611fa545
Eric Kurtz <erickurtz@live.com>
2018-04-20 16:30:13 -0400

    made parser an object. started gui initial ideas.

--- chesscom_test.py
+++ chesscom_test.py
@@ -1,19 +1,16 @@
 import urllib.request
 from bs4 import BeautifulSoup
 import re
-
-
-
-
+import tkinter as tk
 
 class AppURLopener(urllib.request.FancyURLopener):
     version = "Mozilla/5.0"
 
 
 class PlayerTracker:
-    def __init__(self, url):
+    def __init__(self):
         self.opener = AppURLopener()
-        data = []
+        self.data = []
 
 
     def getLatestGameFromURL(self, url):
@@ -27,12 +24,13 @@ class PlayerTracker:
         for row in rows:
             cols = row.find_all('td')
             cols = [ele.text.strip() for ele in cols]
-            data.append([ele for ele in cols if ele]) # Get rid of empty vals
+            self.data.append([ele for ele in cols if ele]) # Get rid of empty vals
             
         return self.parseGameFromString()
 
-    def parseGameFromString():
-        lastgame = str(data[0])
+    def parseGameFromString(self):
+        print ("parsing last game") 
+        lastgame = str(self.data[0])
         lastgame = lastgame.replace('[', "")
         lastgame = lastgame.replace(']', "")
         lastgame = lastgame.replace("'", "")
@@ -45,9 +43,63 @@ class PlayerTracker:
         lastgame = re.sub(r"^(GM |IM |FM |NM |CM |WGM |WIM |WFM |WNM |WCM )", " ", lastgame)
         words = lastgame.split(" ")
         return words
+
+## game list of of format
+## ['natsky555', '(2583)', 'erichansen', '(2731)', '0', '1', '1', 'min', '23', 'Apr', '16', '2018']
+## ['white-name', '(w-rate)', 'black-name', '(b-rate)
+
     
-def __main__():
+class ChessComMatchTracker:
+    def __init__(self, master):
+        self.winsFile = "homeScore.txt"
+        self.lossesFile = "awayScore.txt"
+        self.streakFile = "streak.txt"
+        self.master = master
+        self.start = False
+        self.master.minsize(width=500,height=500)
+        self.tracker = PlayerTracker()
+        self.trackedUrl = ""
+        self.latestGame = []
+        self.previousGame = []        
+        self.homeScore = Tk.DoubleVar()
+        self.awayScore = Tk.DoubleVar()
+        self.homeScore.set(0.0)
+        self.awayScore.set(0.0)
+        self.trackedPlayer = Tk.StringVar()
+        self.opponentPlayer = Tk.StringVar()
+        self.enterPlayer = Tk.Entry(self.master)
+        self.trackedPlayer.set("")
+        self.opponentPlayer.set("")
+        self.startButton = Tk.Button(self.master, text = "Start", command = self.pressStart)
+        self.inputName = Tk.Button(self.master, text = "Enter", command = self.setTrackerPlayer)
+        
+    def parseGame(self, game):
+        print("nothing yet")
+                                       
+    def track(self):
+        if self.start:
+            print("started")
+                       
+    def pressStart(self):
+        if(self.trackerPlayer.get() == ""):
+            print("please enter a player") ##don't actually print anything
+        else:
+            self.start = not(self.start)
+            if self.start:
+                self.startButton["Stop"]
+            else:
+                self.startButton["Start"]
+            
+    def setTrackerPlayer(self):
+        self.trackedPlayer = self.enterPlayer.get()
+        self.trackedUrl = "https://www.chess.com/games/archive?username="+self.trackedPlayer
+            
+            
+def main():
     url = "https://www.chess.com/games/archive?username=erichansen"
-    parser = PlayerTracker(url)
+    parser = PlayerTracker()
+    print (parser.getLatestGameFromURL(url))
+
+main()
     
     
